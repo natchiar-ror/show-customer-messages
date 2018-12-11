6ec2f3ca6e38193fec2b01d97e61a606b279500a
natchiar <natchiar.v@gmail.com>
2018-12-11 06:29:14 +0530

    modifying gemfile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def display_str(audit_log)
+		aud_log = AudLog.find(audit_log)
+		asset = Asset.find(aud_log.asset_id)
+		return asset.html_string
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get "view_message/:asset_id/:customer_id", to: "home#view_message", as: :view_message
+  delete "delete_message/:asset_id", to: "home#delete_message", as: :delete_message
+  resources :configs
+  resources :assets  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
