3c07e56acbc5c3d0b6de11c2f831fe8fe4d361e1
natchiar <natchiar.v@gmail.com>
2018-12-13 23:04:16 +0530

    adding seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |i|
+	Asset.create(asset_id: "At_#{i}",eff_dt: Time.now, html_string: "The text or view message for Asset1 goes here..", ver_nbr: 1, title: "TitleAsst_#{i}", description: "Description Asset_#{i}")
+end
+
+20.times do |j|
+	Config.create(cust_id: j, loan_nbr: "loan_no_#{j}",actv_sts: "T",asset_id: j)
+end
